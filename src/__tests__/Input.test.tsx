729a16dbe9d43af97a658f9f68d61903401cade5
MancunianSam <sam.palmer2@protonmail.com>
2019-03-23 13:01:38 +0000

    Remove useless test

--- src/__tests__/Input.test.tsx
+++ src/__tests__/Input.test.tsx
@@ -7,20 +7,4 @@ describe('<Input />', () => {
     const { container } = render(<Input onClick={jest.fn()} />);
     expect(container).not.toBeNull();
   });
-
-  test('onClick is called with correct text for empty input', async () => {
-    const onClick: jest.Mock = jest.fn();
-    const { getByAltText, getByTestId, getByText } = render(
-      <Input onClick={onClick} />
-    );
-    const textInput: HTMLElement = getByAltText('input');
-    const button: HTMLElement = await waitForElement(() =>
-      getByTestId('inputButton')
-    );
-    fireEvent.change(textInput, { target: { value: 'TEST' } });
-
-    fireEvent.click(button);
-    //TODO Should be called but isn't
-    // expect(onClick).toHaveBeenCalled();
-  });
 });
