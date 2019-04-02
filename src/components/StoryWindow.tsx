ceaf891648edfffad1253be43f1ceb39a1e998ae
MancunianSam <sam.palmer2@protonmail.com>
2019-04-02 15:29:19 +0100

    Style changes

--- src/components/StoryWindow.tsx
+++ src/components/StoryWindow.tsx
@@ -4,14 +4,19 @@ import styled from '@emotion/styled';
 interface IStoryWindowProps {
   image: string;
   position?: string;
+  size?: string;
+  display?: string;
 }
 
 const StoryWindow: React.FunctionComponent<IStoryWindowProps> = styled.div(
   props => ({
     backgroundImage: `url(${props.image})`,
     backgroundPosition: props.position || '0% 0%',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: props.size ? props.size : '',
     gridColumnStart: 2,
-    gridRowStart: 2
+    gridRowStart: 2,
+    position: 'relative'
   })
 );
 
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,13 +3,17 @@ import styled from '@emotion/styled';
 
 interface ITextProps {
   colour?: string;
+  size?: string;
+  alignSelf?: string;
 }
 
 const Text: React.FunctionComponent<ITextProps> = styled.h1`
-  margin-top: 40px;
-  font-size: 50px;
+  font-size: ${props => props.size || '50px'};
   font-family: Open Sans;
   color: ${props => props.colour || 'black'};
+  ${props => (props.alignSelf ? `align-self:${props.alignSelf};` : '')}
+  grid-column: 2;
+  grid-row: 1 / 3;
 `;
 
 export { Text };
